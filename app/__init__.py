5fceee91e994e60bee46a36280dc99e8e79b84ef
Vester1 <korolevsp08@gmail.com>
2023-05-08 19:47:57 +0300

    Commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from . import models, views
 
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+
+from . import models, views
+
 db.create_all()
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ import datetime
 class URLmodel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(255))
-    short = db.Column(db.String(SHORT_LEN), unique=True)
+    short = db.Column(db.String(6), unique=True)
     visits = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
